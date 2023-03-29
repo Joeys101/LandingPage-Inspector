f41741d2ccfb4cc46f5bb4cd8bd057cdd692b0d4
jose sanchez <jose.onepiece@hotmail.com>
2023-03-29 13:09:30 -0300

    Servicios

--- app.js
+++ app.js
@@ -20,17 +20,19 @@ const conocenos = document.getElementById("conocenos")
 const caja = document.getElementById("caja")
 
 function mostrar() {
+  planilla.classList.add("mostrar-animacion");
+  planilla.classList.remove("ocultar-animacion");
   planilla.style.visibility = "visible";
   foto.style.filter = "brightness(0.5)";
-  conocenos.style.display = "none"
-  caja.style.display = "none"
+  conocenos.style.display = "none";
+  caja.style.display = "none";
   nav.style["background-color"] = "rgba(0, 0, 0, 0.274)";
 }
 function ocultar() {
-  planilla.style.visibility = "hidden"
+  planilla.classList.remove("mostrar-animacion");
+  planilla.style.visibility = "hidden";
   foto.style.filter = "brightness(1)";
-  conocenos.style.display = "block"
-  caja.style.display = "flex"
+  conocenos.style.display = "block";
+  caja.style.display = "flex";
   nav.style["background-color"] = "white";
-
 }
